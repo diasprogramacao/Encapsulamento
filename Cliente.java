f3c266ff1aee738bacdc2d353ed594e921ffdeb5
Alan Dias da Silva <79154696+diasprogramacao@users.noreply.github.com>
2021-07-15 19:43:18 -0300

    Adicioando a classe cliente
    Adicionando a classe cliente

---
+++ Cliente.java
@@ -0,0 +1,31 @@
+package contaBancariaEncapsulamento;
+
+public class Cliente {
+	
+	private String nome; 
+	private String endereço;
+	private String profissao;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereço() {
+		return endereço;
+	}
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+	
+	
+	
+}
